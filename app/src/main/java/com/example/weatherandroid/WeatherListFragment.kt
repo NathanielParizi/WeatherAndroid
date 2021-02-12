a2b4d8a82310362b2c90c34a7fbcad46797818e9
Joel Parizi <joelparizi@Joels-MBP.attlocal.net>
2021-02-12 03:57:23 -0500

    ListView is not working properly

--- app/src/main/java/com/example/weatherandroid/WeatherListFragment.kt
+++ app/src/main/java/com/example/weatherandroid/WeatherListFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.weatherandroid.ViewModels.WeatherViewModel
 import com.example.weatherandroid.databinding.FragmentWeatherListBinding
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -13,6 +15,8 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class WeatherListFragment : Fragment() {
 
     val weatherViewModel: WeatherViewModel by viewModel()
+    var listItems = arrayOf("Cloudy", "Rainy", "Sunny")
+
 
     private var _binding: FragmentWeatherListBinding? = null
     private val binding get() = _binding!!  //read only variable
@@ -27,6 +31,15 @@ class WeatherListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val listView = binding.listview
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+
+
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(requireContext(), listItems[position], LENGTH_LONG).show()
+        }
 
 //            findNavController().navigate(R.id.action_weatherListFragment_to_detailsFragment)
 
