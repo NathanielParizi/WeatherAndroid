8d56aad314526aea362de35a1266465794b738ce
Joel Parizi <joelparizi@Joels-MBP.attlocal.net>
2021-02-14 16:41:24 -0500

    Updated Dependency 2' q /q q push q

--- app/src/main/java/com/example/weatherandroid/DataSource/Remote/IApiService.kt
+++ app/src/main/java/com/example/weatherandroid/DataSource/Remote/IApiService.kt
@@ -1,10 +1,8 @@
 package com.example.weatherandroid.DataSource.Remote
 
 import com.example.weatherandroid.Model.WeatherApiResponse
-import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface IApiService {
--- app/src/main/java/com/example/weatherandroid/Model/WeatherApiResponse.kt
+++ app/src/main/java/com/example/weatherandroid/Model/WeatherApiResponse.kt
@@ -1,9 +1,5 @@
 package com.example.weatherandroid.Model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class WeatherApiResponse(
 
     val city: City,
@@ -11,5 +7,5 @@ data class WeatherApiResponse(
     val cod: String,
     val message: Int,
     val list: List<Forecast>
-) : Parcelable
+)
 
